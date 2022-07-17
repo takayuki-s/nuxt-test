8a45c2be6aff8c76ce5df4b3413c4dc29af6d4b6
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-07-18 07:20:59 +0900

    ライフサイクル
    fetchはcreatedの後に読み込まれるが、引数を持たせるとcreatedの前に読み込まれる

---
+++ pages/lifeCycle.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    life cycle
+  </div>
+</template>
+
+<script>
+export default {
+  asyncData() {
+    console.log('asyncData') // eslint-disable-line no-console
+  },
+  fetch( context ) {
+    console.log('fetch') // eslint-disable-line no-console
+    console.log(context.route.name) // eslint-disable-line no-console
+  },
+  created() {
+    console.log('created') // eslint-disable-line no-console
+  },
+}
+</script>

6fcdcba056cbbc7b960e799129bff0cf9a9fdc51
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-07-19 08:07:05 +0900

    contextをいくつかログで表示
    分割代入
    （元はcontext.routeなどになる）

--- pages/lifeCycle.vue
+++ pages/lifeCycle.vue
@@ -6,8 +6,13 @@
 
 <script>
 export default {
-  asyncData() {
+  asyncData( { route, isDev, app, $axios } ) {
     console.log('asyncData') // eslint-disable-line no-console
+    console.log('route.name: ', route.name) // eslint-disable-line no-console
+    console.log('idDev: ', isDev) // eslint-disable-line no-console
+    console.log('app: ', app) // eslint-disable-line no-console
+    console.log('app.$axios: ', app.$axios) // eslint-disable-line no-console
+    console.log('$axios: ', $axios) // eslint-disable-line no-console
   },
   fetch( context ) {
     console.log('fetch') // eslint-disable-line no-console
